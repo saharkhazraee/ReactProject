e57f4fd91cb8c79f412d64da900afa48a12538fa
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-06-24 02:33:07 +0330

    update navbar

--- Client/src/Components/Navbar/index.jsx
+++ Client/src/Components/Navbar/index.jsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../../Store/Slices/Auth'
 import { Box, Button, Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 export default function Navbar() {
     const { token } = useSelector(state => state.auth)
     const dispath = useDispatch()
@@ -10,18 +13,39 @@ export default function Navbar() {
         <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} gap={'10px'}
             sx={{ width: '100%', height: '70px', boxShadow: "0 8px 5px -5px rgba(0, 0, 0, 0.2)", px: '15px' }} component={'nav'}>
             <Stack component={'div'} direction={'row'} justifyContent={'space-between'} alignContent={'center'} gap={'10px'}>
-             <Button variant='text'><Link style={{color:'#878585'}} to={'/'}>Home</Link></Button>
-             <Button variant='text'><Link style={{color:'#878585'}} to={'/woman-products/all-woman-product/all-category'}>Woman</Link></Button>
-             <Button variant='text'><Link style={{color:'#878585'}} to={'/man-products/all-man-product/all-category'}>Man</Link></Button>
-             <Button variant='text'><Link style={{color:'#878585'}} to={'/casual-products'}>Casual Style</Link></Button>
-             <Button variant='text'><Link style={{color:'#878585'}} to={'/about'}>About</Link></Button>
+                <Button variant='text'><Link style={{ color: '#878585' }} to={'/'}>Home</Link></Button>
+                <Button variant='text'><Link style={{ color: '#878585' }} to={'/woman-products/all-woman-product/all-category'}>Woman</Link></Button>
+                <Button variant='text'><Link style={{ color: '#878585' }} to={'/man-products/all-man-product/all-category'}>Man</Link></Button>
+                <Button variant='text'><Link style={{ color: '#878585' }} to={'/casual-products'}>Casual Style</Link></Button>
+                <Button variant='text'><Link style={{ color: '#878585' }} to={'/about'}>About</Link></Button>
             </Stack>
             <Stack component={'div'} direction={'row'}>
-            <Typography><Box component={'span'} >S</Box>AHAR</Typography>
+                <Typography><Box component={'span'} >S</Box>AHAR</Typography>
             </Stack>
-            
+
             <Stack component={'div'} direction={'row'} justifyContent={'space-between'} alignContent={'center'} gap={'10px'}>
 
+                {token ?
+                    <Button variant='text' onClick={() => dispath(logout())}>
+                        <Typography component={'span'} sx={{ color: '#878585' }}>
+                            Log out
+                        </Typography>
+                    </Button>
+
+                    :
+                    <Button variant='text' onClick={() => dispath(logout())}>
+                        <Link style={{ color: '#878585' }} to={'/auth'}>
+                            <PermIdentityOutlinedIcon  />
+                        </Link>
+                    </Button>
+                }
+                <Button variant='text' onClick={() => dispath(logout())}>
+                    <Link style={{ color: '#878585' }} to={'/cart'}>
+                        <Badge>
+                            <ShoppingCartIcon />
+                        </Badge>
+                    </Link>
+                </Button>
             </Stack>
 
         </Stack>
---
+++ Client/src/Pages/Home/Banner/index.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './style.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import fetchData from '../../../Utils/FetchData';
+
+export default function Banner() {
+    const [slide, setSlide] = useState()
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+        
+    };
+    useEffect(()=>{
+        (async()=>{
+            const res=await fetchData('sliders?populate=*')
+            return setSlide(res.data)
+        })()
+    },[])
+    const items = slide?.map((e, index) => <SwiperSlide key={index}>
+        <img src={import.meta.env.VITE_BASE_URL+} alt=''/>
+    </SwiperSlide>)
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                onAutoplayTimeLeft={onAutoplayTimeLeft}
+                className="banner"
+            >
+
+                {items}
+                <div className="autoplay-progress" slot="container-end">
+                    <svg viewBox="0 0 48 48" ref={progressCircle}>
+                        <circle cx="24" cy="24" r="20"></circle>
+                    </svg>
+                    <span ref={progressContent}></span>
+                </div>
+            </Swiper>
+        </>
+    );
+}
