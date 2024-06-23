889e7f2dbb87e830cd99c1d960f2ffc04c5a82eb
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-06-23 23:34:25 +0330

    write app.jsx & main

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,39 @@
-
 import React from 'react'
-
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import { Box } from '@mui/material'
+import { useSelector } from 'react-redux'
+import Home from './Pages/Home'
+import WomanProducts from './Pages/WomanProducts'
+import ManProducts from './Pages/ManProducts'
+import CasualProducts from './Pages/CasualProducts'
+import ProductDetails from './Pages/ProductDetails'
+import Cart from './Pages/Cart'
+import Auth from './Pages/Auth'
+import About from './Pages/About'
+import Search from './Pages/Search'
+import NotFound from './Pages/NotFound'
 export default function App() {
+  const {token}=useSelector(state=>state.auth)
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <Box minHeight={'80vh'}>
+        <Routes>
+         <Route exact path='/' element={<Home/>}/>
+         <Route path='/woman-products/:categoryId/:categoryName' element={<WomanProducts/>}/>
+         <Route path='/man-products/:categoryId/:categoryName' element={<ManProducts/>}/>
+         <Route path='/casual-products' element={<CasualProducts/>}/>
+         <Route path='/product-details' element={<ProductDetails/>}/>
+         <Route path='/auth' element={!token?<Auth/>:<Navigate to={'/'}/>}/>
+         <Route path='cart' element={token?<Cart/>:<Navigate to={'/auth'}/>}/>
+         <Route path='about' element={<About/>}/>
+         <Route path='search/:query' element={<Search/>}/>
+         <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </Box>
+      <Footer />
+    </>
   )
 }
