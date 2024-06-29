2ca9f5e716ddc4aca22f0b046fd2d997d74377ed
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-06-29 15:34:21 +0330

    footer

--- Client/src/Components/Footer/index.jsx
+++ Client/src/Components/Footer/index.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <Stack justifyContent={'space-between'} alignItems={'center'} gap={'20px'} flexWrap={'wrap'}  width={'100%'}
-    sx={{padding:'5%',color:'#8B4513'}} direction={'row'}>
-      <Stack justifyContent={'center'} alignItems={'center'} gap={'20px'}>
-        <Typography variant=''></Typography>
+    <Stack justifyContent={'space-between'} alignItems={'center'} gap={'20px'} flexWrap={'wrap'} width={'100%'}
+      sx={{ padding: '5%', bgcolor: '#8B4513 !important' }} direction={'row'}>
+      <Stack justifyContent={'center'}  gap={'20px'} >
+        <Typography variant='h4' component={'h1'} sx={{color:'white',textAlign:'left !important'}}>SAHAR</Typography>
+        <Typography variant='body2' component={'span'} sx={{color:'white',textAlign:'left'}}>WhatsApp:+989360644520</Typography>
+        <Typography variant='body2' component={'span'} sx={{color:'white',textAlign:'left'}}>Email:s.khazraee_88@yahoo.com</Typography>
+        <Typography variant='body2' component={'span'} sx={{color:'white',textAlign:'left'}}>Address:Mashad daneshju Bulvar</Typography>
       </Stack>
-      <Stack></Stack>
-      <Stack></Stack>
-      <Stack></Stack>
+     
     </Stack>
   )
 }
